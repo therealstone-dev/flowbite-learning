de772fa8d5974f249945edb5ada70976c11dea07
Jesus Barrios <236945161+therealstone-dev@users.noreply.github.com>
2026-05-11 15:02:28 +0000

    xdzhbnfawn

--- flowbite-flask/app.py
+++ flowbite-flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,14 @@ app = Flask(__name__)
 @app.route("/index")
 def index():
 	return render_template("index.html")
+
+@app.route('/procesar_formulario', methods=['POST'])
+def procesar_formulario():
+
+
+	email = request.form.get('email', '')
+	password = request.form.get('password', '')
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
